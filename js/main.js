d55a546850cadc430c86d3616f979ea8f96be4d1
TemnovKZ <114981979+TemnovKZ@users.noreply.github.com>
2023-01-24 16:18:41 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 let switchToggle = document.querySelector('.switch__btn');
 let price = document.querySelectorAll('.price__cost');
 let sale = 20;
+let initialPrice = Array.from(price).map(item => +item.innerText);
 
 switchToggle.onclick = () => {
     switchToggle.classList.toggle('active');
-    if(switchToggle.classList.contains('active')){
-        for(var i = 0; i < price.length; i++) {
-            price[i].innerText = Math.round(price[i].innerHTML * 12 * (100 - sale) / 100);
-        }
-    }else{
-        for(var k = 0; k < price.length; k++) {
-            price[k].innerText = price[k].innerHTML / 12 / (100 - sale) * 100;
-        }
-    }
+     if(switchToggle.classList.contains('active')){
+         for(var i = 0; i < price.length; i++) {
+             price[i].innerText = Math.round(initialPrice[i] * 12 * (100 - sale) / 100);
+         }
+     }else{
+         for(var k = 0; k < price.length; k++) {
+             price[k].innerText = price[k].innerHTML / 12 / (100 - sale) * 100;
+             price[k].innerText = initialPrice[k];
+         }
+     }
 }
